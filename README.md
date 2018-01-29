0e4647ed27f141972c034a95943a2eee05c10a8c
Vladimir <vladimir.cherepanov@cern.ch>
2018-01-29 13:29:38 +0100

    fix todo

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # PhIIG4setup
+
+Quick Instruction:
+run ./todo.pl and source created file Install_G4Env_ ...
+
+PhaseIIBeam_v1.tar.gz is provided by Patrick Asenov (patrickasenov@gmail.com)
--- todo.pl
+++ todo.pl
@@ -20,21 +20,18 @@ $SLDP='\$G4DIR';
 $SPWD='\$PWD';
 $time= strftime("%h_%d_%Y",localtime);
 
-system(sprintf("rm Install_TauolaEnvironment_*"));
+system(sprintf("rm Install_G4Env_*"));
 
 system(sprintf("echo \"/libcern/use/use.py geant4;\">> Install_G4Env_$time"));
+system(sprintf("echo \"source /libcern/root/5.34.18/sl6.3-x86_64/setup.sh \n\">> Install_G4Env_$time"));
 system(sprintf("echo \"cd /libcern/geant4/4.10.03-p02/sl6.6-x86_64-gate/bin/;\">> Install_G4Env_$time"));
 system(sprintf("echo \"source geant4.sh;\">> Install_G4Env_$time"));
 system(sprintf("echo \"cd $PWD;\">> Install_G4Env_$time"));
-	
 system(sprintf("echo \"export G4INSTALL=/libcern/geant4/4.10.03-p02/geant4.10.03.p02/;\">> Install_G4Env_$time"));
-
-system(sprintf("echo \"cp -r  /libcern/geant4/4.10.03-p02/sl6.6-x86_64-gate/share/Geant4-10.3.2/examples/basic/B1 .\">> Install_G4Env_$time"));
-system(sprintf("echo \"mkdir B3build\">> Install_G4Env_$time"));
-
-
-system(sprintf("echo \"cd B3build/\">> Install_G4Env_$time"));
-system(sprintf("echo \"cmake -DGeant4_DIR=$SLDP ../B1/\">> Install_G4Env_$time"));
-
+system(sprintf("echo \"gunzip -d PhaseIIBeam_v1.tar.gz\">> Install_G4Env_$time"));
+system(sprintf("echo \"tar -xvf PhaseIIBeam_v1.tar\">> Install_G4Env_$time"));
+system(sprintf("echo \"mkdir PhaseIIBeam_v1-build\">> Install_G4Env_$time"));
+system(sprintf("echo \"cd PhaseIIBeam_v1-build\">> Install_G4Env_$time"));
+system(sprintf("echo \"cmake -DGeant4_DIR=$SLDP ../PhaseIIBeam_v1/\">> Install_G4Env_$time"));
 system(sprintf("echo \" make -j2\">> Install_G4Env_$time"));
 
